bf58b6041fd418ec270f64b4c620812c0f6c0f9a
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-03-31 10:54:42 -0500

    Actualizar catalogadminmodal.tsx

--- catalogadminmodal.tsx
+++ catalogadminmodal.tsx
@@ -1,6 +1,15 @@
-Type '(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void' is not assignable to type 'ChangeEventHandler<HTMLSelectElement>'.
-  Types of parameters 'e' and 'event' are incompatible.
-    Type 'ChangeEvent<HTMLSelectElement>' is not assignable to type 'ChangeEvent<HTMLInputElement | HTMLTextAreaElement>'.
-      Type 'HTMLSelectElement' is not assignable to type 'HTMLInputElement | HTMLTextAreaElement'.
-        Type 'HTMLSelectElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, capture, and 38 more.ts(2322)
-index.d.ts(3342, 9): The expected type comes from property 'onChange' which is declared here on type 'DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>'
+const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  if (!editandoCurso) return;
+  const { name, value } = e.target;
+
+  setEditandoCurso((prev) => {
+    if (!prev) return null;
+
+    const camposNumericos = ["Valor", "Publico", "Horas", "Linea", "Estado", "Modalidad", "Unidad", "Profesor", "IdTipoCurso"];
+
+    return {
+      ...prev,
+      [name]: camposNumericos.includes(name) ? parseInt(value) || 0 : value,
+    };
+  });
+};
