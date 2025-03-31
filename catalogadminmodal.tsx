27b95c437830bdd96f46985b32f38f9db8e2fb7e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-03-31 10:52:34 -0500

    Update catalogadminmodal.tsx

--- catalogadminmodal.tsx
+++ catalogadminmodal.tsx
@@ -1,31 +1,6 @@
-{Object.keys(editandoCurso).map((key) => (
-  key !== "id" && (
-    <div key={key} className="mt-2">
-      <label className="block text-sm font-bold">{key}</label>
-
-      {key === "Publico" ? (
-        // Si el campo es "Publico", usa un <select> en lugar de un <input>
-        <select
-          name="Publico"
-          value={editandoCurso.Publico}
-          onChange={handleChange}
-          className="border p-2 w-full rounded"
-        >
-          {opcionesPublico.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-      ) : (
-        <input
-          type="text"
-          name={key}
-          value={editandoCurso ? (editandoCurso as any)[key] ?? "" : ""}
-          onChange={handleChange}
-          className="border p-2 w-full rounded"
-        />
-      )}
-    </div>
-  )
-))}
+Type '(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void' is not assignable to type 'ChangeEventHandler<HTMLSelectElement>'.
+  Types of parameters 'e' and 'event' are incompatible.
+    Type 'ChangeEvent<HTMLSelectElement>' is not assignable to type 'ChangeEvent<HTMLInputElement | HTMLTextAreaElement>'.
+      Type 'HTMLSelectElement' is not assignable to type 'HTMLInputElement | HTMLTextAreaElement'.
+        Type 'HTMLSelectElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, capture, and 38 more.ts(2322)
+index.d.ts(3342, 9): The expected type comes from property 'onChange' which is declared here on type 'DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>'
