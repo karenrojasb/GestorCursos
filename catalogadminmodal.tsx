989e47f372e49dfcf7546c63a0eecf8a09ca4e95
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-01 08:01:51 -0500

    Update catalogadminmodal.tsx

--- catalogadminmodal.tsx
+++ catalogadminmodal.tsx
@@ -38,11 +38,6 @@ interface Curso {
   DomingoIni: string;
   DomingoFin: string;
 }
-interface Opcion {
-  id: number;
-  Especificacion: string;
-  Tipo: number;
-}
 
 export default function CatalogoModal({ onClose }: { onClose: () => void }) {
   
@@ -51,18 +46,8 @@ export default function CatalogoModal({ onClose }: { onClose: () => void }) {
   const [expandedCursoId, setExpandedCursoId] = useState<number | null>(null);
   const [busqueda, setBusqueda] = useState("");
   const [isSearchActive, setIsSearchActive] = useState(false);
-  const [editandoCurso, setEditandoCurso] = useState<Curso | null>(null);
   const [mensajeExito, setMensajeExito] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [opcionesPublico, setOpcionesPublico] = useState<Opcion[]>([]);
-  const [opcionesLinea, setOpcionesLinea] = useState<Opcion[]>([]);
-  const [opcionesModalidad, setOpcionesModalidad] = useState<Opcion[]>([]);
-  const [opcionesEstado, setOpcionesEstado] = useState<Opcion[]>([]);
-  const [opcionesTipoCurso, setOpcionesTipoCurso] = useState<Opcion[]>([]);
-  const [profesores, setProfesores] = useState<{ id_emp: number; nombre: string}[]>([]);
-  const [unidad, setUnidad] = useState<{ codigo: number; nombre: string}[]>([])
-  const [opcionesPeriodos, setOpcionesPeriodos] = useState<{ periodo: string}[]>([]);
-
  
 
   // OBTENER CURSO DE BACKEND
@@ -83,73 +68,6 @@ export default function CatalogoModal({ onClose }: { onClose: () => void }) {
   useEffect(() => {
     fetchCursos();
   }, []);
-
-   useEffect(() => {
-      async function fetchOpciones() {
-        try {
-          const response = await fetch("http://localhost:8090/api/cursos/especificaciones");
-          if (!response.ok) throw new Error("Error al obtener las opciones");
-  
-          const data: Opcion[] = await response.json();
-  
-          setOpcionesPublico(data.filter((item) => item.Tipo === 1));
-          setOpcionesLinea(data.filter((item) => item.Tipo === 2));
-          setOpcionesModalidad(data.filter((item) => item.Tipo === 3));
-          setOpcionesEstado(data.filter((item) => item.Tipo === 4));
-          setOpcionesTipoCurso(data.filter((item) => item.Tipo === 8));
-        } catch (error) {
-          console.error("Error cargando las opciones:", error);
-        }
-      }
-  
-      fetchOpciones();
-    }, []);
-
-    useEffect(()  => {
-      async function fetcProfesores() {
-        try {
-          const response = await fetch("http://localhost:8090/api/cursos/profesores")
-          if (!response.ok) throw new Error("Error al obtener los profesores");
-  
-          const data = await response.json();
-          setProfesores(data);
-          } catch(error){
-            console.error("Error cargando los profesores:", error);
-          }
-        }
-        fetcProfesores(); 
-    }, []);
-
-    useEffect(()  => {
-      async function fetcPeriodos() {
-        try {
-          const response = await fetch("http://localhost:8090/api/cursos/periodos")
-          if (!response.ok) throw new Error("Error al obtener los periodos");
-  
-          const data = await response.json();
-          setOpcionesPeriodos(data);
-          } catch(error){
-            console.error("Error cargando lista de periodos:", error);
-          }
-        }
-        fetcPeriodos(); 
-    }, []);
-  
-  
-    useEffect(()  => {
-      async function fetcUnidades() {
-        try {
-          const response = await fetch("http://localhost:8090/api/cursos/unidad")
-          if (!response.ok) throw new Error("Error al obtener las unidades");
-  
-          const data = await response.json();
-          setUnidad(data);
-          } catch(error){
-            console.error("Error cargando lista de unidades:", error);
-          }
-        }
-        fetcUnidades(); 
-    }, []);
 
 
 
@@ -213,67 +131,8 @@ export default function CatalogoModal({ onClose }: { onClose: () => void }) {
     setIsLoading(false);
   };
 
-  // INICIAR EDICIÓN
-  const handleEditar = (curso: Curso) => {
-    setEditandoCurso({ ...curso });
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    if (!editandoCurso) return;
-    const { name, value } = e.target;
-  
-    setEditandoCurso((prev) => {
-      if (!prev) return null;
-  
-      const camposNumericos = ["Valor", "Publico", "Horas", "Linea", "Estado", "Modalidad", "Unidad", "Profesor", "IdTipoCurso"];
-  
-      return {
-        ...prev,
-        [name]: camposNumericos.includes(name) ? parseInt(value) || 0 : value,
-      };
-    });
-  };
-
-// GUARDAR CAMBIOS AL EDITAR
-const handleGuardarEdicion = async () => {
-  if (!editandoCurso) return;
-  console.log("Intentando guardar cambios...");
-  console.log("Datos enviados al backend:", JSON.stringify(editandoCurso, null, 2));
 
 
-    setIsLoading(true);
-    try {
-      const response = await fetch(`http://localhost:8090/api/cursos/${editandoCurso.id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(editandoCurso),
-      });
-
-      if (!response.ok) {
-        console.error("Error HTTP al actualizar:", response.status);
-        const errorText = await response.text();
-        console.error("Respuesta del servidor:", errorText)
-        return;
-      }
-
-      console.log("Curso actualizado con éxito");
-      
-      setMensajeExito("¡Curso actualizado con éxito!");
-
-      // RECARGAR DESPUES DE LA ACTUALIZACIÓN DEL CURSO
-      await fetchCursos();
-
-      // CERRAR MODA DE EDICIÓN
-      setTimeout(()=> {
-        setMensajeExito("");
-        setEditandoCurso(null);
-      }, 3000);
-      
-    } catch (error) {
-      console.error("Error al guardar la edición:", error);
-    }
-    setIsLoading(false);
-  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -342,7 +201,7 @@ const handleGuardarEdicion = async () => {
                    
                     {/* BOTÓN PARA EDITAR */}
                     <button 
-                    onClick={() => handleEditar(curso)} 
+                   
                     className="bg-[#990000] hover:bg-red-700 text-white p-2 rounded transition-transform hover:scale-110 active:scale-95"
                     title="Editar">
                       <PencilSquareIcon className="h-5 w-5" />
@@ -456,169 +315,8 @@ const handleGuardarEdicion = async () => {
       
       
 
-       {/* MODAL EDICIÓN DE CURSO */}
- {editandoCurso && (
-   <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-     <div className="relative bg-white p-6 rounded-lg shadow-lg max-h-[80vh] overflow-y-auto w-full max-w-md">
       
-       {/* BOTÓN PARA CERRAR */}
-       <button className="absolute top-4 right-4 text-gray-500 hover:text-[#990000] transition-transform duration-300 hover:rotate-90" 
-         onClick={() => setEditandoCurso(null)}>
-         <XMarkIcon className="w-6 h-6" />
-       </button>
-
-       <h2 className="text-lg text-[#990000] font-bold mb-4">Editar Curso</h2>
-       {/* MENSAJE DE ÉXITO AL ACTUALIZAR EL CURSO*/}
-       {mensajeExito && (
-         <div className="fixed top-10 left-1/2 transform -translate-x-1/2 bg-[#990000] text-white p-2 rounded text-center mb-4">
-           {mensajeExito}
-         </div>
-       )}
-       
-
-       {/* FORMULARIO DE EDICIÓN */}
-       {Object.keys(editandoCurso).map((key) => (
-  key !== "id" && (
-    <div key={key} className="mt-2">
-      <label className="block text-sm font-bold">{key}</label>
-
-      {key === "Publico" ? (
-        // Si el campo es "Publico", usa un <select> en lugar de un <input>
-        <select
-          name="Publico"
-          value={editandoCurso.Publico}
-          onChange={handleChange}
-          className="border p-2 w-full rounded"
-        >
-          {opcionesPublico.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-
-        
-      ) : key === "Linea" ? (
-        <select
-        name={key}
-        value={editandoCurso.Linea}
-        onChange={handleChange}
-        className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-        
-          {opcionesLinea.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-
-         ) : key === "Modalidad" ? (
-        <select
-        name={key}
-        value={editandoCurso.Modalidad}
-        onChange={handleChange}
-        className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-        
-          {opcionesModalidad.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-
-         ) : key === "Estado" ? (
-        <select
-        name={key}
-        value={editandoCurso.Estado}
-        onChange={handleChange}
-        className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-        
-          {opcionesEstado.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-
-         ) : key === "IdTipoCurso" ? (
-        <select
-        name={key}
-        value={editandoCurso.IdTipoCurso}
-        onChange={handleChange}
-        className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-        
-          {opcionesTipoCurso.map((opcion) => (
-            <option key={opcion.id} value={opcion.id}>
-              {opcion.Especificacion}
-            </option>
-          ))}
-        </select>
-
-        ) : key === "Profesor" || key === "SegundoPro" ? (
-       <select
-       name={key}
-       value={editandoCurso.Profesor}
-       onChange={handleChange}
-       className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-   
-          {profesores.map((profesor) => (
-            <option key={profesor.id_emp} value={profesor.id_emp}>
-             {profesor.nombre}
-             </option>
-          ))}
-        </select>
-
-         ) : key === "Unidad" ? (
-                <select
-                name={key}
-                value={editandoCurso.Unidad}
-                onChange={handleChange}
-                className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-                  
-                  {unidad.map((unidad) => (
-                    <option key={unidad.codigo} value={unidad.codigo}>
-                      {unidad.nombre}
-                    </option>
-                  ))}
-                </select>
-
-         ) : key === "Periodo" ? (
-          <select
-          name={key}
-          value={editandoCurso.Periodo}
-          onChange={handleChange}
-          className="w-full border p-2 rounded-lg focus:ring-2 focus:ring-[#990000]">
-            
-            {opcionesPeriodos.map((opcion, index) => (
-              <option key={index} value={opcion.periodo}>
-                {opcion.periodo}
-              </option>
-            ))}
-          </select>
-  ) : (
-        
-        <input
-          type="text"
-          name={key}
-          value={editandoCurso ? (editandoCurso as any)[key] ?? "" : ""}
-          onChange={handleChange}
-          className="border p-2 w-full rounded"
-        />
-      )}
-    </div>
-  )
-))}
-    
-
-       {/* BOTONES GUARDAR Y CANCELAR */}
-       <div className="mt-4 flex space-x-4 ">
-         <button onClick={handleGuardarEdicion} className="bg-[#990000] text-white px-4 py-2 rounded">Guardar</button>
-         <button onClick={() => setEditandoCurso(null)} className="bg-gray-700 text-white px-4 py-2 rounded">Cancelar</button>
-       </div>
-            
-           </div>
-         </div>
-       )}
+ 
      </div>
    );
  }
