9d4ceb105ed8365d3ffb30f8cc66cdc7eeb5583f
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-03 13:41:42 -0500

    Update InscripcionesModat.tsx

--- InscripcionesModat.tsx
+++ InscripcionesModat.tsx
@@ -1,20 +1,10 @@
-const handleBuscar = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const texto = e.target.value.toLowerCase();
-  setBusqueda(texto);
-
-  const filtrados = inscripciones.filter((inscripcion) => {
-    const cursoNombre = inscripcion.Cursos?.NombreCurso || inscripcion.curso?.NombreCurso || inscripcion.NombreCurso || "";
-    const fechaRegistro = new Date(inscripcion.fecreg).toLocaleDateString();
-    const idCurso = inscripcion.idCur?.toString() || inscripcion.Cursos?.id?.toString() || inscripcion.curso?.id?.toString() || "";
-
-    return (
-      idCurso.includes(texto) ||
-      cursoNombre.toLowerCase().includes(texto) || // Aquí se incluye el nombre del curso en la búsqueda
-      fechaRegistro.includes(texto) ||
-      inscripcion.nombre.toLowerCase().includes(texto) ||
-      inscripcion.docInscr.includes(texto)
-    );
-  });
-
-  setInscripcionesFiltradas(filtrados);
-};
+TypeError: Cannot read properties of null (reading 'toLowerCase')
+    at http://localhost:3000/_next/static/chunks/app_b72629._.js:3071:65
+    at Array.filter (<anonymous>)
+    at handleBuscar (http://localhost:3000/_next/static/chunks/app_b72629._.js:3066:41)
+    at processDispatchQueue (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8501:25)
+    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8798:13
+    at batchedUpdates$1 (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:2090:44)
+    at dispatchEventForPluginEventSystem (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8583:9)
+    at dispatchEvent (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:10685:37)
+    at dispatchDiscreteEvent (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:10667:64)
