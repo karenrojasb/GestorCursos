fe8430baaa1b73f504fce1d223ca6554913bd9fc
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-04 14:46:13 -0500

    Actualizar catalogadminmodal.tsx

--- catalogadminmodal.tsx
+++ catalogadminmodal.tsx
@@ -1,10 +1,8 @@
-<div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="animate-check-spin scale-125">
-              {esInscripcion ? (
-                <CheckCircleIcon className="h-32 w-32 text-green-500" />
-              ) : (
-                <CheckCircleIcon className="h-32 w-32 text-green-500" />
-              )}
-            </div>
-            <p className="text-white text-2xl font-bold mt-2 animate-fade-in">{mensajeAnimacion}</p>
-          </div>
+{showSuccess && (
+  <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-[9999]">
+    <div className="animate-check-spin scale-125">
+      <CheckCircleIcon className="h-32 w-32 text-green-500" />
+    </div>
+    <p className="text-white text-2xl font-bold mt-2 animate-fade-in">Curso actualizado</p>
+  </div>
+)}
