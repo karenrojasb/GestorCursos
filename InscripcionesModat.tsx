3641d876044d4266d56ed353c0ab349671f43277
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-09 08:55:27 -0500

    Actualizar InscripcionesModat.tsx

--- InscripcionesModat.tsx
+++ InscripcionesModat.tsx
@@ -4,3 +4,11 @@ const especificacion = notaEncontrada?.Listas?.Especificacion;
 {especificacion && especificacion !== "Sin calificar"
   ? especificacion
   : "Sin Nota"}
+
+
+const notaEncontrada = nota.find(n => String(n.idInscrito).trim() === String(inscripciones.docInscr).trim());
+const especificacion = notaEncontrada?.Listas?.Especificacion;
+
+{especificacion && especificacion !== "Sin calificar"
+  ? especificacion
+  : "Sin Nota"}
