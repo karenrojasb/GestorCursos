c655239026887ac1b307cfa8d3b31a321ca618ec
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-09 09:19:17 -0500

    Actualizar InscripcionesModat.tsx

--- InscripcionesModat.tsx
+++ InscripcionesModat.tsx
@@ -1,10 +1,16 @@
 
-{(() => {
-    const notaEncontrada = nota.find(n => String(n.idInscrito).trim() === String(inscripciones.docInscr).trim());
-    const especificacion = notaEncontrada?.Listas?.Especificacion;
-    return (especificacion && especificacion !== "Sin calificar")
-      ? especificacion
-      : "Sin Nota";
-  })()}
-  
-  
+useEffect(()  => {
+  async function fetcNotas() {
+    try {
+      const response = await fetch("http://localhost:8090/api/Notas");
+      if (!response.ok) throw new Error("Error al obtener los periodos");
+
+      const data = await response.json();
+      console.log("Notas recibidas:", data);  // <-- Agrega esto
+      setNota(data);
+    } catch(error){
+      console.error("Error cargando lista de periodos:", error);
+    }
+  }
+  fetcNotas(); 
+}, []);
