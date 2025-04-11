d7a9ff8eb392e2d4ee6c7094d56c80e699b73d3d
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-11 16:38:23 -0500

    Update InscripcionesModat.tsx

--- InscripcionesModat.tsx
+++ InscripcionesModat.tsx
@@ -46,6 +46,15 @@ export default function InscripcionesModal({ onClose }: InscripcionesModalProps)
   const [nota, setNota] = useState<any[]>([]);
   const [modalCalificarAbierto, setModalCalificarAbierto] = useState(false);
   const [inscritoSeleccionado, setInscritoSeleccionado] = useState<{nombre: string, doc:string} | null>(null);
+  const [idEmp, setIdEmp] = useState<number | null>(null);
+
+  useEffect(() => {
+    const storedIdEmp = localStorage.getItem("id_emp");
+    if (storedIdEmp) {
+      setIdEmp(Number(storedIdEmp));
+      
+    }
+  }, []);
 
   useEffect(() => {
     const fetchInscripciones = async () => {
@@ -421,3 +430,45 @@ const guardarNota = async (notaTexto: string) => {
     </div>
   );
 }
+
+  async getCursosPorProfesor(idProfesor: number) {
+    return this.prisma.$queryRawUnsafe<
+      Array<{
+        id: number;
+        idCur: number;
+        NombreCurso: string;
+        Profesor: number;
+        SegundoPro: number;
+        Lugar: string;
+        Inicio: Date;
+        Fin: Date;
+        docInscr: string;
+        nombre: string | null;
+        fecreg: Date;
+        rol: string; // 'Titular' o 'Segundo'
+      }>
+    >(
+      `SELECT 
+        i.id,
+        i.idCur,
+        c.NombreCurso,
+        c.Profesor,
+        c.SegundoPro,
+        c.Lugar,
+        c.Inicio,
+        c.Fin,
+        i.docInscr,
+        e.nombre,
+        i.fecreg,
+        CASE 
+          WHEN c.Profesor = ${idProfesor} THEN 'Titular'
+          WHEN c.SegundoPro = ${idProfesor} THEN 'Segundo'
+          ELSE 'Otro'
+        END AS rol
+      FROM gescur.Cursos c
+      LEFT JOIN gescur.Inscripciones i ON i.idCur = c.id
+      LEFT JOIN gescur.emp_nomina e ON i.docInscr = e.id_emp
+      WHERE c.Profesor = ${idProfesor} OR c.SegundoPro = ${idProfesor}`
+    );
+  }
+  
