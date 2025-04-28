ce6de27453141af76f8f23d2160ee155fc08fd0b
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-28 15:16:40 -0500

    Actualizar Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,6 +1,6 @@
 useEffect(() => {
-  const storedIdEmp = localStorage.getItem('idEmp');
+  const storedIdEmp = localStorage.getItem('id_emp'); // O como hayas guardado el id
   if (storedIdEmp) {
-    setIdEmp(Number(storedIdEmp));
+    setIdEmp(Number(storedIdEmp)); // Guarda el id_emp en el estado
   }
 }, []);
