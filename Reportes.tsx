7204c0bea82fe1e29e32f4e1ce1f91cdb1ad083d
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-28 15:45:43 -0500

    Actualizar Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,1 +1,23 @@
-This comparison appears to be unintentional because the types 'string' and 'number' have no overlap.ts(2367)
+const fetchCursos = async () => {
+  setIsLoading(true);
+  try {
+    const response = await fetch(`http://localhost:8090/api/cursos`);
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+    const data: Curso[] = await response.json();
+
+    if (idEmp !== null) {
+      const idEmpStr = idEmp.toString(); // Convertimos a string
+      const cursosFiltradosPorProfesor = data.filter(
+        curso => curso.Profesor === idEmpStr || curso.SegundoPro === idEmpStr
+      );
+      setCursos(cursosFiltradosPorProfesor);
+      setCursosFiltrados(cursosFiltradosPorProfesor);
+    } else {
+      setCursos(data);
+      setCursosFiltrados(data);
+    }
+  } catch (error) {
+    console.error("Error al obtener los cursos:", error);
+  }
+  setIsLoading(false);
+};
