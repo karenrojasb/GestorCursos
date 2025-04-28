7247a95d47d2294fa66205b01b338e58521ef9d8
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-28 15:38:23 -0500

    Update Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,12 +1,23 @@
-useEffect(() => {
-  const storedIdEmp = localStorage.getItem('id_emp'); 
-  if (storedIdEmp) {
-    setIdEmp(Number(storedIdEmp)); 
-  }
-}, []);
 
-useEffect(() => {
-  if (idEmp !== null) {
-    fetchCursos();
-  }
-}, [idEmp]);
+  const fetchCursos = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`http://localhost:8090/api/cursos`);
+      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+      const data: Curso[] = await response.json();
+  
+      // Si hay idEmp, filtrar los cursos donde Profesor coincida
+      if (idEmp !== null) {
+        const cursosFiltradosPorProfesor = data.filter(curso => curso.Profesor === idEmp);
+        setCursos(cursosFiltradosPorProfesor);
+        setCursosFiltrados(cursosFiltradosPorProfesor);
+      } else {
+        setCursos(data);
+        setCursosFiltrados(data);
+      }
+    } catch (error) {
+      console.error("Error al obtener los cursos:", error);
+    }
+    setIsLoading(false);
+  };
+  
