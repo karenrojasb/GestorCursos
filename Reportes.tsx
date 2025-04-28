8558d411cc3b91d723d7f8b5e3217f4762ac877c
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-04-28 15:42:12 -0500

    Actualizar Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,23 +1,22 @@
+const fetchCursos = async () => {
+  setIsLoading(true);
+  try {
+    const response = await fetch(`http://localhost:8090/api/cursos`);
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+    const data: Curso[] = await response.json();
 
-  const fetchCursos = async () => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(`http://localhost:8090/api/cursos`);
-      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
-      const data: Curso[] = await response.json();
-  
-      // Si hay idEmp, filtrar los cursos donde Profesor coincida
-      if (idEmp !== null) {
-        const cursosFiltradosPorProfesor = data.filter(curso => curso.Profesor === idEmp);
-        setCursos(cursosFiltradosPorProfesor);
-        setCursosFiltrados(cursosFiltradosPorProfesor);
-      } else {
-        setCursos(data);
-        setCursosFiltrados(data);
-      }
-    } catch (error) {
-      console.error("Error al obtener los cursos:", error);
+    if (idEmp !== null) {
+      const cursosFiltradosPorProfesor = data.filter(
+        curso => curso.Profesor === idEmp || curso.SegundoPro === idEmp
+      );
+      setCursos(cursosFiltradosPorProfesor);
+      setCursosFiltrados(cursosFiltradosPorProfesor);
+    } else {
+      setCursos(data);
+      setCursosFiltrados(data);
     }
-    setIsLoading(false);
-  };
-  
+  } catch (error) {
+    console.error("Error al obtener los cursos:", error);
+  }
+  setIsLoading(false);
+};
