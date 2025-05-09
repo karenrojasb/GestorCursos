6e7848624280f19cb74aede9506f2ec09d7f3722
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-09 08:47:30 -0500

    Update Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,24 +1,10 @@
-const handleBuscar = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const texto = e.target.value.toLowerCase();
-  setBusqueda(texto);
-
-  const filtrados = inscripciones.filter((inscripcion) => {
-    const cursoNombre = inscripcion.NombreCurso?.toLowerCase() || "";
-    const fechaRegistro = inscripcion.fecreg
-      ? new Date(inscripcion.fecreg).toLocaleDateString().toLowerCase()
-      : "";
-    const idCurso = String(inscripcion.idCur || "").toLowerCase();
-    const nombreInscrito = inscripcion.nombre?.toLowerCase() || "";
-    const docInscrito = inscripcion.docInscr?.toLowerCase() || "";
-
-    return (
-      idCurso.includes(texto) ||
-      cursoNombre.includes(texto) ||
-      fechaRegistro.includes(texto) ||
-      nombreInscrito.includes(texto) ||
-      docInscrito.includes(texto)
-    );
-  });
-
-  setInscripcionesFiltradas(filtrados);
-};
+TypeError: inscripcion.docInscr?.toLowerCase is not a function
+    at eval (http://localhost:3000/_next/static/chunks/app_744e9d._.js?id=%255Bproject%255D%252Fapp%252Fuser%252FReportes%252Fpage.tsx+%255Bapp-client%255D+%2528ecmascript%2529:73:55)
+    at Array.filter (<anonymous>)
+    at handleBuscar (http://localhost:3000/_next/static/chunks/app_744e9d._.js?id=%255Bproject%255D%252Fapp%252Fuser%252FReportes%252Fpage.tsx+%255Bapp-client%255D+%2528ecmascript%2529:68:41)
+    at processDispatchQueue (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8501:25)
+    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8798:13
+    at batchedUpdates$1 (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:2090:44)
+    at dispatchEventForPluginEventSystem (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8583:9)
+    at dispatchEvent (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:10685:37)
+    at dispatchDiscreteEvent (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:10667:64)
