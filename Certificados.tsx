5d44397c9959e52f71803723eaeef9564519f221
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-09 08:33:22 -0500

    Actualizar Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,32 +1,35 @@
-import * as XLSX from 'xlsx';
+const handleExportCurso = (cursoId: number) => {
+  const curso = cursos.find((c) => c.curso.id === cursoId);
+  if (!curso) return;
 
-const exportarCursoConInscritos = (curso: any, inscritos: any[]) => {
-  // Hoja 1: Información del curso
-  const hojaCurso = XLSX.utils.json_to_sheet([{
-    Nombre: curso.nombre,
-    Profesor: curso.profesor,
-    Lugar: curso.lugar,
-    Horario: curso.horario,
-    Fechas: `${curso.fechaInicio} - ${curso.fechaFin}`,
-    // ...otros campos
-  }]);
+  const dataToExport: any[] = curso.inscripciones.map((inscripcion: any) => ({
+    Nombre: inscripcion.nombre,
+    Documento: inscripcion.docInscr,
+    Fecha_Registro: inscripcion.fecreg,
+    Nota: inscripcion.Nota ?? "No calificado",
+    Curso: curso.curso.nombre,
+    Profesor: curso.curso.profesor,
+    Segundo_Profesor: curso.curso.segundoProfesor,
+    Lugar: curso.curso.lugar,
+    Horario: curso.curso.horario,
+    Fecha_Inicio: curso.curso.fechaInicio,
+    Fecha_Fin: curso.curso.fechaFin,
+    Estado: curso.curso.estado,
+  }));
 
-  // Hoja 2: Lista de inscritos
-  const hojaInscritos = XLSX.utils.json_to_sheet(
-    inscritos.map(ins => ({
-      Documento: ins.docInscr,
-      Nombre: ins.nombre,
-      FechaRegistro: ins.fecreg,
-      Nota: ins.nota,
-      // ...otros campos
-    }))
-  );
-
-  // Crear libro y añadir ambas hojas
+  const worksheet = XLSX.utils.json_to_sheet(dataToExport);
   const workbook = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(workbook, hojaCurso, 'Curso');
-  XLSX.utils.book_append_sheet(workbook, hojaInscritos, 'Inscritos');
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Curso");
 
-  // Exportar
-  XLSX.writeFile(workbook, `Curso_${curso.nombre}.xlsx`);
+  XLSX.writeFile(workbook, `curso_${cursoId}.xlsx`);
 };
+
+
+<Button
+  variant="outline"
+  size="sm"
+  className="ml-auto"
+  onClick={() => handleExportCurso(cursoGroup.curso.id)}
+>
+  Exportar Excel
+</Button>
