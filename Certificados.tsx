67cae2a85baa1b437f8be94405b2df277208b8d0
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-09 12:35:52 -0500

    Update Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,15 +1,10 @@
-               <div className="relative flex items-center"
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}>
-                   <button onClick={() => setIsSearchActive(!isSearchActive)} className="p-2 rounded-full bg-gray-200">
-                     <MagnifyingGlassIcon className="h-6 w-6 text-[#990000]" />
-                   </button>
-                   <input
-                     type="text"
-                     placeholder="Busque el nombre del curso"
-                     value={busqueda}
-                     onChange={handleBuscar}
-                     className={`px-4 w-full py-2 border rounded-full transition-all duration-500 ease-in-out 
-                       ${isSearchActive ? "w-96 opacity-100 bg-white shadow-md" : "w-0 opacity-0"} focus:outline-none`}
-                   /> 
-                   </div>
+ <div className="flex items-center gap-2">
+                    <span className="text-black">{insc.Especificacion || 'Sin Nota'}</span>
+                    <button
+                      onClick={() => abrirModalCalificar(insc.nombre, insc.docInscr)}
+                      className="flex items-center gap-1 px-2 py-1 bg-[#990000] text-white text-xs rounded-md hover:bg-red-800 transition-all duration-200 "
+                    >
+                      <PlusCircleIcon className="w-4 h-4" />
+                      Calificar
+                    </button>
+                  </div>
