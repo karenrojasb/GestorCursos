47ae5c393f11caecc25d7a11b5f985168603f90c
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-13 09:37:19 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -69,10 +69,9 @@ export default function CalificarModal({
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-         
+          idCur,
           idInscrito: documento,
           Nota: notaSeleccionada,
-          idcurso:idCur,
         }),
       });
 
@@ -89,7 +88,7 @@ export default function CalificarModal({
   };
 
   return (
-    <div className="seleccion-personalizada fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-md w-[400px] relative">
       <button
             onClick={onClose}
