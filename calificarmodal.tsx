42bd0eeed7ff20f4ed657bf84f0ec68785d06083
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-13 11:25:36 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,38 +1,6 @@
-const handleGuardar = async () => {
-  if (notaSeleccionada === null) {
-    alert("Por favor selecciona una nota");
-    return;
-  }
-
-  setGuardando(true);
-
-  try {
-    const response = await fetch("http://localhost:8090/api/Notas", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        idCurso: idCur, // Se sube como idCurso
-        idInscrito: documento, // Se sube como idInscrito
-        idRegistro: idEmp, // Se sube el id_emp como idRegistro
-        Nota: notaSeleccionada, // La nota seleccionada
-      }),
-    });
-
-    // Revisar el status de la respuesta y su cuerpo
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("Error detallado al guardar la nota:", errorData);
-      throw new Error("Error al guardar la nota");
-    }
-
-    onGuardar(String(notaSeleccionada));
-    onClose();
-  } catch (error) {
-    console.error("Error al guardar nota:", error);
-    alert("Hubo un error al guardar la nota.");
-  } finally {
-    setGuardando(false);
-  }
-};
+Error: Error detallado al guardar la nota: {}
+    at createUnhandledError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:689:49)
+    at handleClientError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:856:56)
+    at console.error (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:991:56)
+    at handleGuardar (http://localhost:3000/_next/static/chunks/app_744e9d._.js:3265:25) Error: Error al guardar la nota
+    at handleGuardar (http://localhost:3000/_next/static/chunks/app_744e9d._.js:3266:23)
