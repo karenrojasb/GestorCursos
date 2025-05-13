5dadad2e6380a77e4769706d76872e87e38161da
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-13 15:43:42 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,3 +1,23 @@
+// MÉTODO PARA CREAR NOTA
+async createNote(CreateNotaDto: CreateNotaDto) {
+  try {
+    const newNote = await this.prisma.notas.create({
+      data: {
+        idCurso: CreateNotaDto.idCurso,
+        idInscrito: CreateNotaDto.idInscrito,
+        Nota: CreateNotaDto.Nota,
+        idRegistro: CreateNotaDto.idRegistro,
+        FechaRegistro: new Date(),
+      },
+    });
+    return newNote;
+  } catch (error) {
+    console.error('Error al crear la nota:', error);
+    throw new Error(`No se pudo crear la nota: ${error.message}`);
+  }
+}
+
+
 
 import { useState, useEffect } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
