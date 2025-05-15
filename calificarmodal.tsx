2f07908d1f02d9cf17cb6c1809da92ab48c6d4b8
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-15 17:04:52 -0500

    Actualizar calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,6 +1,8 @@
 <select
   className="w-full border rounded px-3 py-2"
-  value={Especificacion ?? ""}
+  value={
+    opciones.find((op) => op.Especificacion === Especificacion)?.id ?? ""
+  }
   onChange={(e) => setNotaSeleccionada(Number(e.target.value))}
 >
   <option value="">-- Selecciona --</option>
