09a07626d61757ce17c10c4bc0ceba9e76af4ab3
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-15 14:19:01 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,409 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { CreateNotaDto } from './dto/create-nota.dto';
-import { UpdateNotaDto } from './dto/update-nota.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { Prisma } from '@prisma/client';
-
-@Injectable()
-export class NotasService {
-  updateNote(arg0: number, UpdateNotaDto: UpdateNotaDto) {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private readonly prisma: PrismaService) {}
-
-//  OBTENER TODAS LAS NOTAS
-
-async findAll() {
-  const notas = await this.prisma.notas.findMany({
-    include: {
-      Listas: {
-        select: {
-          Especificacion: true,
-        },
-      },
-    },
-  });
-
-  const notasConInscrito = await Promise.all(notas.map(async (nota) => {
-    const inscrito = await this.prisma.inscripciones.findFirst({
-      where: {
-        docInscr: nota.idInscrito,
-      },
-      select: {
-        
-        docInscr: true,
-      },
+  //  ACTUALIZAR EL ESTADO DEL INSCRITO
+  async getState(id: number, dto: UpdateInscripcionDto) {
+    return this.prisma.inscripciones.update({
+      where: { id },
+      data: { est: Boolean(dto.est) },
     });
-
-    return {
-      ...nota,
-      inscrito: inscrito || null,
-    };
-  }));
-
-  return notasConInscrito;
-}
-
-
-// MÉTODO PARA CREAR NOTA
-async createNote(CreateNotaDto: CreateNotaDto) {
-  console.log('DTO recibido:', CreateNotaDto); // <--- esto es importante
-
-  try {
-    const newNote = await this.prisma.notas.create({
-      data: {
-        idCurso: CreateNotaDto.idCurso,
-        idInscrito: CreateNotaDto.idInscrito,
-        Nota: CreateNotaDto.Nota,
-        idRegistro: CreateNotaDto.idRegistro,
-        FechaRegistro: new Date(),
-      },
-    });
-    return newNote;
-  } catch (error) {
-    console.error('Error al crear la nota:', error);
-    throw new Error(`No se pudo crear la nota: ${error.message}`);
-  }
-}
-
-
-
-    // OBTENER NOTA POR ID INSCRITO 
-    async getWrittenNote(idInscrito: number) {
-      const notas = await this.prisma.notas.findMany({
-        where: { idInscrito },
-        include: {
-          Listas: {
-            select: {
-              Especificacion: true,
-            },
-          },
-        },
-      });
-    
-      const notasConCurso = await Promise.all(
-        notas.map(async (nota) => {
-          if (!nota.idCurso) {
-            return {
-              ...nota,
-              idCurso: null,
-              NombreCurso: null,
-              Lugar: null,
-              Inicio: null,
-              Fin: null,
-              LunesIni: null,
-              LunesFin: null,
-              MartesIni: null,
-              MartesFin: null,
-              MiercolesIni: null,
-              MiercolesFin: null,
-              JuevesIni: null,
-              JuevesFin: null,
-              ViernesIni: null,
-              ViernesFin: null,
-              SabadoIni: null,
-              SabadoFin: null,
-              Profesor: null,
-              Proexterno: null,
-              SegundoPro: null,
-              ProfesorNombre: null,
-              SegundoProNombre: null,
-            };
-          }
-    
-          const curso = await this.prisma.cursos.findFirst({
-            where: {
-              id: nota.idCurso,
-            },
-            select: {
-              NombreCurso: true,
-              Lugar: true,
-              Inicio: true,
-              Fin: true,
-              LunesIni: true,
-              LunesFin: true,
-              MartesIni: true,
-              MartesFin: true,
-              MiercolesIni: true,
-              MiercolesFin: true,
-              JuevesIni: true,
-              JuevesFin: true,
-              ViernesIni: true,
-              ViernesFin: true,
-              SabadoIni: true,
-              SabadoFin: true,
-              Profesor: true,
-              SegundoPro: true,
-              Proexterno: true,
-            },
-          });
-    
-          const profesorId = curso?.Profesor?.toString() || '0';
-          const segundoProId = curso?.SegundoPro?.toString() || '0';
-    
-          const [profesor] = await this.prisma.$queryRaw<
-            { nombre: string }[]
-          >`SELECT nombre FROM gescur.emp_nomina WHERE id_emp = CAST(${profesorId} AS VARCHAR)`;
-    
-          const [segundoPro] = await this.prisma.$queryRaw<
-            { nombre: string }[]
-          >`SELECT nombre FROM gescur.emp_nomina WHERE id_emp = CAST(${segundoProId} AS VARCHAR)`;
-    
-          return {
-            ...nota,
-            NombreCurso: curso?.NombreCurso || null,
-            Lugar: curso?.Lugar || null,
-            Inicio: curso?.Inicio || null,
-            Fin: curso?.Fin || null,
-            LunesIni: curso?.LunesIni || null,
-            LunesFin: curso?.LunesFin || null,
-            MartesIni: curso?.MartesIni || null,
-            MartesFin: curso?.MartesFin || null,
-            MiercolesIni: curso?.MiercolesIni || null,
-            MiercolesFin: curso?.MiercolesFin || null,
-            JuevesIni: curso?.JuevesIni || null,
-            JuevesFin: curso?.JuevesFin || null,
-            ViernesIni: curso?.ViernesIni || null,
-            ViernesFin: curso?.ViernesFin || null,
-            SabadoIni: curso?.SabadoIni || null,
-            SabadoFin: curso?.SabadoFin || null,
-            ProExterno: curso?.Proexterno || null,
-            Profesor: curso?.Profesor || null,
-            SegundoPro: curso?.SegundoPro || null,
-            ProfesorNombre: profesor?.nombre ?? null,
-            SegundoProNombre: segundoPro?.nombre ?? null,
-          };
-        })
-      );
-    
-      return notasConCurso;
-    }
-
-    
-// OBTENER NOTAS APROVADAS POR IDINSCRITO 
-async getAprovado(idInscrito: number | string) {
-  const idInscritoInt = Number(idInscrito);
-  if (isNaN(idInscritoInt)) {
-    throw new Error('El ID del inscrito debe ser un número válido');
-  }
-
-  const notas = await this.prisma.notas.findMany({
-    where: {
-      idInscrito: idInscritoInt,
-      Nota: 33,
-    },
-    include: {
-      Listas: {
-        select: {
-          Especificacion: true,
-        },
-      },
-    },
-  });
-
-  const notasConCurso = await Promise.all(
-    notas.map(async (nota) => {
-      if (!nota.idCurso) {
-        return {
-          ...nota,
-          NombreCurso: null,
-          Lugar: null,
-          Inicio: null,
-          Fin: null,
-          LunesIni: null,
-          LunesFin: null,
-          MartesIni: null,
-          MartesFin: null,
-          MiercolesIni: null,
-          MiercolesFin: null,
-          JuevesIni: null,
-          JuevesFin: null,
-          ViernesIni: null,
-          ViernesFin: null,
-          SabadoIni: null,
-          SabadoFin: null,
-          Profesor: null,
-          Proexterno: null,
-          SegundoPro: null,
-          ProfesorNombre: null,
-          SegundoProNombre: null,
-        };
-      }
-
-      const curso = await this.prisma.cursos.findFirst({
-        where: {
-          id: nota.idCurso,
-        },
-        select: {
-          NombreCurso: true,
-          Lugar: true,
-          Inicio: true,
-          Fin: true,
-          LunesIni: true,
-          LunesFin: true,
-          MartesIni: true,
-          MartesFin: true,
-          MiercolesIni: true,
-          MiercolesFin: true,
-          JuevesIni: true,
-          JuevesFin: true,
-          ViernesIni: true,
-          ViernesFin: true,
-          SabadoIni: true,
-          SabadoFin: true,
-          Profesor: true,
-          SegundoPro: true,
-          Proexterno: true,
-        },
-      });
-
-      const profesorId = curso?.Profesor?.toString() || '0';
-      const segundoProId = curso?.SegundoPro?.toString() || '0';
-
-      const [profesor] = await this.prisma.$queryRaw<
-        { nombre: string }[]
-      >`SELECT nombre FROM gescur.emp_nomina WHERE id_emp = CAST(${profesorId} AS VARCHAR)`;
-
-      const [segundoPro] = await this.prisma.$queryRaw<
-        { nombre: string }[]
-      >`SELECT nombre FROM gescur.emp_nomina WHERE id_emp = CAST(${segundoProId} AS VARCHAR)`;
-
-      return {
-        ...nota,
-        NombreCurso: curso?.NombreCurso || null,
-        Lugar: curso?.Lugar || null,
-        Inicio: curso?.Inicio || null,
-        Fin: curso?.Fin || null,
-        LunesIni: curso?.LunesIni || null,
-        LunesFin: curso?.LunesFin || null,
-        MartesIni: curso?.MartesIni || null,
-        MartesFin: curso?.MartesFin || null,
-        MiercolesIni: curso?.MiercolesIni || null,
-        MiercolesFin: curso?.MiercolesFin || null,
-        JuevesIni: curso?.JuevesIni || null,
-        JuevesFin: curso?.JuevesFin || null,
-        ViernesIni: curso?.ViernesIni || null,
-        ViernesFin: curso?.ViernesFin || null,
-        SabadoIni: curso?.SabadoIni || null,
-        SabadoFin: curso?.SabadoFin || null,
-        ProExterno: curso?.Proexterno || null,
-        Profesor: curso?.Profesor || null,
-        SegundoPro: curso?.SegundoPro || null,
-        ProfesorNombre: profesor?.nombre ?? null,
-        SegundoProNombre: segundoPro?.nombre ?? null,
-      };
-    }),
-  );
-
-  return notasConCurso;
-}
-
-
-    // MÉTODO PARA ACTUALIZAR
-    async UpdateNote (id: number, data: Prisma.NotasUpdateInput){
-      console.log ('id received:', id);
-      console.log ('data received:', data);
-
-      try {
-        const UpdateNote = await this.prisma.notas.update({
-          where: {id},
-          data,
-        });
-        console.log ('update note:', UpdateNote);
-        return UpdateNote;
-      }
-      catch (error){
-        console.error ('error when updating note:', error);
-        throw new error('the note was not updated');
-      }
-    }
-
-    // MÉTODO PARA ELIMINAR
-    async deleteNote(id:number){
-      return this.prisma.notas.delete({where: {id}});
-    }
-
-    // Obtener una nota por idCurso e idInscrito
-async findByCursoAndInscrito(idCurso: number, idInscrito: number) {
-  return this.prisma.notas.findFirst({
-    where: {
-      idCurso,
-      idInscrito,
-    },
-  });
-}
-
-}
-
-
-
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, Put, Query } from '@nestjs/common';
-import { NotasService } from './notas.service';
-import { CreateNotaDto } from './dto/create-nota.dto';
-import { UpdateNotaDto } from './dto/update-nota.dto';
-
-
-@Controller('notas')
-export class NotasController {  
-  constructor(private readonly notasService: NotasService) {}
-
-//  OBTENER DATOS
-@Get()
-async findAll() {
-  return this.notasService.findAll();
-}
-
-
-
-  // CREAR DATOS
-  @Post()
-  createNote (@Body() CreateNotaDto: CreateNotaDto){
-    return this.notasService.createNote(CreateNotaDto);
   }
 
 
-  // OBTENER NOTA POR ID DEL INSCRITO
-  @Get(':idInscrito')
-async getWrittenNote(@Param('idInscrito') idInscrito: string) {
-  return this.notasService.getWrittenNote(Number(idInscrito));
-}
 
-// OBTENER APROVADOS POR ID DEL INSCRITO
-@Get('aprovado/:idInscrito')
-async getAprovado(@Param('idInscrito') idInscrito: number) {
-  return this.notasService.getAprovado(idInscrito);
-}
-
-  // ACTUALIZAR DATOS
-  @Put(':id')
-  async updateNote(@Param('id') id:string, @Body() UpdateNotaDto: UpdateNotaDto){
-    const updateNote = await this.notasService.UpdateNote(Number(id), UpdateNotaDto)
-    if (!updateNote){
-      throw new NotFoundException(`The id#${id} not found`);
-    }
-    return updateNote;
-  } 
-
-  // MÉTODO PARA ELIMINAR
-  @Delete(':id')
-  async deleteNote (@Param('id') id:string){
-    return this.notasService.deleteNote(Number(id));
-  }
-
-  
-  @Get('curso-inscrito')
-  async getNotaPorCursoEInscrito(
-    @Query('idCurso') idCurso: number,
-    @Query('idInscrito') idInscrito: number
-  ) {
-    return this.notasService.findByCursoAndInscrito(Number(idCurso), Number(idInscrito));
+  // ACTUALIZAR EL ESTADO DEL INSCRITO
+  @Patch(':id')
+  getState(@Param('id') id: string, @Body() dto: UpdateInscripcionDto) {
+    return this.inscripcionesService.getState(Number(id), dto);
   }
 
 
-}
 
-
-
+Frontend
 import { useState, useEffect } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
@@ -411,6 +24,8 @@ interface CalificarModalProps {
   nombre: string;
   documento: number;
   idCur: number;
+  Especificacion: string;
+  idIns: number;
   onClose: () => void;
   onGuardar: (nota: string) => void;
 }
@@ -424,6 +39,8 @@ export default function CalificarModal({
   nombre,
   documento,
   idCur,
+  Especificacion,
+  idIns,
   onClose,
   onGuardar,
 }: CalificarModalProps) {
@@ -543,29 +160,25 @@ export default function CalificarModal({
 
         <p className="text-left mb-2"><strong>Nombre del Estudiante:</strong> {nombre}</p>
         <p className="text-left mb-2"><strong>Documento:</strong> {documento}</p>
-
-        {notaSeleccionada !== null && (
-          <p className="text-left mb-2">
-            <strong>Nota actual:</strong> {especificacionNota || "Sin especificación"}
-          </p>
-        )}
-
+        
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-700 mb-1">
             Selecciona una calificación:
           </label>
           <select
-            className="w-full border rounded px-3 py-2"
-            value={notaSeleccionada ?? ""}
-            onChange={(e) => setNotaSeleccionada(Number(e.target.value))}
-          >
-            <option value="">-- Selecciona --</option>
-            {opciones.map((op) => (
-              <option key={op.id} value={op.id}>
-                {op.Especificacion}
-              </option>
-            ))}
-          </select>
+  className="w-full border rounded px-3 py-2"
+  value={Especificacion ?? ""}
+  onChange={(e) => setNotaSeleccionada(Number(e.target.value))}
+>
+  {Especificacion === null && (
+    <option value="">-- Selecciona --</option>
+  )}
+  {opciones.map((op) => (
+    <option key={op.id} value={op.id}>
+      {op.Especificacion}
+    </option>
+  ))}
+</select>
         </div>
 
         <div className="flex justify-center gap-4">
