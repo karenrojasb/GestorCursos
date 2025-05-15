bb8669b2b79a744b15391d5a2625992621b2f652
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-15 16:48:37 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,12 +1,19 @@
-<select
-  className="w-full border rounded px-3 py-2"
-  value={notaSeleccionada ?? ""}
-  onChange={(e) => setNotaSeleccionada(Number(e.target.value))}
->
-  <option value="">-- Selecciona --</option>
-  {opciones.map((op) => (
-    <option key={op.id} value={op.id}>
-      {op.Especificacion}
-    </option>
-  ))}
-</select>
+async createNote(CreateNotaDto: CreateNotaDto) {
+  console.log('DTO recibido:', CreateNotaDto); // <--- esto es importante
+
+  try {
+    const newNote = await this.prisma.notas.create({
+      data: {
+        idCurso: CreateNotaDto.idCurso,
+        idInscrito: CreateNotaDto.idInscrito,
+        Nota: CreateNotaDto.Nota,
+        idRegistro: CreateNotaDto.idRegistro,
+        FechaRegistro: new Date(),
+      },
+    });
+    return newNote;
+  } catch (error) {
+    console.error('Error al crear la nota:', error);
+    throw new Error(`No se pudo crear la nota: ${error.message}`);
+  }
+}
