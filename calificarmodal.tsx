0abf11f3ac1cdb59b3342a1064da6094fe22440d
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-15 16:33:59 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,47 +1,9 @@
-const handleGuardar = async () => {
-  if (notaSeleccionada === null || isNaN(notaSeleccionada)) {
-    alert("Por favor selecciona una nota válida");
-    return;
-  }
-
-  if (!idEmp) {
-    alert("Error: ID de empleado no encontrado.");
-    return;
-  }
-
-  setGuardando(true);
-
-  try {
-    // Si ya existe una nota, no la duplicamos
-    if (notaExistenteId) {
-      console.warn("Ya existe una nota, no se enviará un nuevo POST.");
-      alert("Este estudiante ya tiene una nota asignada.");
-    } else {
-      const responsePost = await fetch("http://localhost:8090/api/notas", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          idCurso: idCur,
-          idInscrito: documento,
-          idRegistro: idEmp,
-          Nota: notaSeleccionada,
-          FechaRegistro: new Date(),
-        }),
-      });
-
-      if (!responsePost.ok) {
-        throw new Error("Error al crear la nota.");
-      }
-
-      onGuardar(String(notaSeleccionada));
-      onClose();
-    }
-  } catch (error) {
-    console.error("Error al guardar nota:", error);
-    alert("Hubo un error al guardar la nota.");
-  } finally {
-    setGuardando(false);
-  }
-};
+id	idCurso	idInscrito	Nota	idRegistro	FechaRegistro
+364	455	1000020661	33	1000034366	2025-05-15
+365	NULL	1000020661	33	1	2025-05-15
+366	450	1000034366	32	1000034366	2025-05-15
+367	NULL	1000034366	32	1	2025-05-15
+368	460	1032402874	33	1000034366	2025-05-15
+369	NULL	1032402874	33	1	2025-05-15
+370	455	1000034366	33	1000034366	2025-05-15
+371	NULL	1000034366	33	1	2025-05-15
