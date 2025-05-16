b914f6883ea51e4156cd728602e444789b5cb8aa
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-16 11:40:52 -0500

    Actualizar calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,12 +1,2 @@
-<td className="border p-2 text-center">
-  <button
-    onClick={() => handleInscripcion(curso.id, !!inscripcion, inscripcion?.id)}
-    className={`${
-      inscripcion
-        ? "bg-[#990000] hover:bg-red-700 text-white"
-        : "bg-green-600 hover:bg-green-700 text-white"
-    } px-3 py-1 rounded text-sm min-w-[120px] transition-colors duration-300`}
-  >
-    {inscripcion ? "Cancelar" : "Inscribirse"}
-  </button>
-</td>
+INSERT INTO gescur.auditoriaNotas (IdCurso, IdInscrito, Nota, IdRegistro, FechaRegistro)
+VALUES (101, 2025, 33, 1, GETDATE());
