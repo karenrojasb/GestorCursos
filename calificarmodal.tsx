8db8bd2bc83fbc45f3b43a5c04c5defec621d312
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-16 11:26:07 -0500

    Actualizar calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,13 +1,12 @@
-  
-                      <td className="border p-2 text-center">
-                        <button
-                          onClick={() => handleInscripcion(curso.id, !!inscripcion, inscripcion?.id)}
-                          className={`${
-                            inscripcion
-                            ?  "bg-[#990000] hover:bg-red-700 text-white text-lg"
-                            : "bg-green-600  hover:bg-green-700 text-white text-lg"
-                          } px-3 py-2 rounded transition-colors duration-300  hover:scale-110 active:scale-95`}
-                        >
-                          {inscripcion ? "Cancelar Inscripción" : "Inscribirse"}
-                        </button>
-                      </td>
+<td className="border p-2 text-center">
+  <button
+    onClick={() => handleInscripcion(curso.id, !!inscripcion, inscripcion?.id)}
+    className={`${
+      inscripcion
+        ? "bg-[#990000] hover:bg-red-700 text-white"
+        : "bg-green-600 hover:bg-green-700 text-white"
+    } px-4 py-1 rounded text-sm w-40 transition-colors duration-300`}
+  >
+    {inscripcion ? "Cancelar Inscripción" : "Inscribirse"}
+  </button>
+</td>
