a77761ac0360f33c47225c11420dc0f408fe41db
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-20 16:58:59 -0500

    Update Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,5 +1,66 @@
-{formatearHorario(curso).map((h) => (
-  <div key={`${curso.id}-${h.dia}`}>
-    <strong>{h.dia}</strong> {h.ini} - {h.fin}
-  </div>
-))}
+// OBTENER CURSOS CON INSCRITOS 
+async getCourses() {
+  return this.prisma.$queryRaw`
+  SELECT 
+    c.id,
+    c.NombreCurso,
+    c.Valor,
+    lp.Especificacion AS Publico,
+    c.Periodo,
+    c.Inicio,
+    c.Fin,
+    c.InicioInscr,
+    c.FinInscr,
+    c.Horas,
+    c.CupoMax,
+    c.Lugar,                          
+    c.LunesIni,                          
+    c.LunesFin,                         
+    c.MartesIni,                       
+    c.MartesFin,                        
+    c.MiercolesIni,                      
+    c.MiercolesFin,                      
+    c.JuevesIni,                         
+    c.JuevesFin,                         
+    c.ViernesIni,                        
+    c.ViernesFin,                        
+    c.SabadoIni,                         
+    c.SabadoFin,                         
+    c.DomingoIni,                        
+    c.DomingoFin,                        
+    c.Linea,
+    l.Especificacion AS Linea,    
+    c.Estado,
+    est.Especificacion AS Estado, 
+    c.Modalidad,
+    m.Especificacion AS Modalidad,
+    u.nombre AS Unidad,                            
+    c.Profesor,                          
+    c.SegundoPro,
+    sp.nombre AS SegundoPro,                        
+    c.Proexterno,                       
+    c.Descripcion,                       
+    c.IdTipoCurso,
+    tc.Especificacion AS IdTipoCurso,
+    e.nombre AS NombreProfesor,
+
+    -- Datos de los inscritos
+    i.id AS InscripcionId,
+    i.docInscr,
+    i.est AS EstadoInscripcion,
+    i.fecreg AS FechaRegistro
+
+  FROM gescur.cursos c
+  LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
+  LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
+  LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
+  LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
+  LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
+  LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
+  LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
+  LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
+
+  -- Join a Inscripciones
+  LEFT JOIN gescur.Inscripciones i ON i.idCur = c.id;
+  `
+}
