89ad26cc8d894864b62d51d4f563a78bc96c679b
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-20 16:51:04 -0500

    Update CursoModal.tsx

--- CursoModal.tsx
+++ CursoModal.tsx
@@ -1,66 +1,19 @@
-// OBTENER CURSOS CON INSCRITOS (sin JSON_AGG)
-async getCourses() {
-  return this.prisma.$queryRaw`
-  SELECT 
-    c.id,
-    c.NombreCurso,
-    c.Valor,
-    lp.Especificacion AS Publico,
-    c.Periodo,
-    c.Inicio,
-    c.Fin,
-    c.InicioInscr,
-    c.FinInscr,
-    c.Horas,
-    c.CupoMax,
-    c.Lugar,                          
-    c.LunesIni,                          
-    c.LunesFin,                         
-    c.MartesIni,                       
-    c.MartesFin,                        
-    c.MiercolesIni,                      
-    c.MiercolesFin,                      
-    c.JuevesIni,                         
-    c.JuevesFin,                         
-    c.ViernesIni,                        
-    c.ViernesFin,                        
-    c.SabadoIni,                         
-    c.SabadoFin,                         
-    c.DomingoIni,                        
-    c.DomingoFin,                        
-    c.Linea,
-    l.Especificacion AS Linea,    
-    c.Estado,
-    est.Especificacion AS Estado, 
-    c.Modalidad,
-    m.Especificacion AS Modalidad,
-    u.nombre AS Unidad,                            
-    c.Profesor,                          
-    c.SegundoPro,
-    sp.nombre AS SegundoPro,                        
-    c.Proexterno,                       
-    c.Descripcion,                       
-    c.IdTipoCurso,
-    tc.Especificacion AS IdTipoCurso,
-    e.nombre AS NombreProfesor,
-
-    -- Datos de los inscritos
-    i.id AS InscripcionId,
-    i.docInscr,
-    i.est AS EstadoInscripcion,
-    i.fecreg AS FechaRegistro
-
-  FROM gescur.cursos c
-  LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
-  LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
-  LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
-  LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
-  LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
-  LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
-  LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
-  LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
-
-  -- Join a Inscripciones
-  LEFT JOIN gescur.Inscripciones i ON i.idCur = c.id;
-  `
-}
+Error: Encountered two children with the same key, `450`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.
+    at createUnhandledError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:689:49)
+    at handleClientError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:856:56)
+    at console.error (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:991:56)
+    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4184:33
+    at runWithFiberInDEV (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:631:20)
+    at warnOnInvalidKey (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4183:21)
+    at reconcileChildrenArray (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4209:160)
+    at reconcileChildFibersImpl (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4315:117)
+    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4342:39
+    at reconcileChildren (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4627:119)
+    at beginWork (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:5413:1573)
+    at runWithFiberInDEV (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:631:20)
+    at performUnitOfWork (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7955:97)
+    at workLoopSync (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7847:40)
+    at renderRootSync (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7830:13)
+    at performWorkOnRoot (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7565:211)
+    at performWorkOnRootViaSchedulerTask (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8394:9)
+    at MessagePort.performWorkUntilDeadline (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_107ce8._.js:2353:64)
