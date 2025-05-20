7f0cb440755c60bd43b4d301e64e8c38b2c8c5bd
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-20 16:13:26 -0500

    Update CursoModal.tsx

--- CursoModal.tsx
+++ CursoModal.tsx
@@ -1,19 +1,23 @@
-Error: Encountered two children with the same key, `12`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.
-    at createUnhandledError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:689:49)
-    at handleClientError (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:856:56)
-    at console.error (http://localhost:3000/_next/static/chunks/node_modules_next_dist_client_523921._.js:991:56)
-    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4184:33
-    at runWithFiberInDEV (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:631:20)
-    at warnOnInvalidKey (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4183:21)
-    at reconcileChildrenArray (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4209:160)
-    at reconcileChildFibersImpl (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4315:117)
-    at http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4342:39
-    at reconcileChildren (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:4627:51)
-    at beginWork (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:5413:1573)
-    at runWithFiberInDEV (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:631:20)
-    at performUnitOfWork (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7955:97)
-    at workLoopSync (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7847:40)
-    at renderRootSync (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7830:13)
-    at performWorkOnRoot (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:7565:211)
-    at performWorkOnRootViaSchedulerTask (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_react-dom_1f56dc._.js:8394:9)
-    at MessagePort.performWorkUntilDeadline (http://localhost:3000/_next/static/chunks/node_modules_next_dist_compiled_107ce8._.js:2353:64)
+async getCursosConInscritos() {
+  try {
+    const resultado = await this.prisma.$queryRaw`
+      SELECT 
+        n.id,
+        n.IdCurso,
+        c.NombreCurso,          
+        n.IdInscrito,
+        n.Nota,
+        n.idRegistro,
+        n.FechaRegistro,
+        i.fecreg
+      FROM gescur."Notas" n
+      LEFT JOIN gescur.cursos c ON n.IdCurso = c.id
+      LEFT JOIN gescur.Inscripciones i ON n.IdInscrito = i.docInscr
+    `;
+
+    return resultado;
+  } catch (error) {
+    console.error("Error al obtener cursos con inscritos:", error.message);
+    throw new Error("No se pudo obtener la lista de cursos con inscritos.");
+  }
+}
