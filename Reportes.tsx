e46371bfa9ed890eb9e0c2a1059d29b9169c8a1e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 10:31:26 -0500

    Update Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,79 +1,41 @@
-// OBTENER CURSOS
-async getCourses() {
-  return this.prisma.$queryRawUnsafe(`
-    SELECT 
-      c.id,
-      c.NombreCurso,
-      c.Valor,
-      lp.Especificacion AS Publico,
-      c.Periodo,
-      c.Inicio,
-      c.Fin,
-      c.InicioInscr,
-      c.FinInscr,
-      c.Horas,
-      c.CupoMax,
-      c.Lugar,                          
-      c.LunesIni,                          
-      c.LunesFin,                         
-      c.MartesIni,                       
-      c.MartesFin,                        
-      c.MiercolesIni,                      
-      c.MiercolesFin,                      
-      c.JuevesIni,                         
-      c.JuevesFin,                         
-      c.ViernesIni,                        
-      c.ViernesFin,                        
-      c.SabadoIni,                         
-      c.SabadoFin,                         
-      c.DomingoIni,                        
-      c.DomingoFin,                        
-      c.Linea,
-      l.Especificacion AS Linea,    
-      c.Estado,
-      est.Especificacion AS Estado, 
-      c.Modalidad,
-      m.Especificacion AS Modalidad,
-      u.nombre AS Unidad,                            
-      c.Profesor,                          
-      c.SegundoPro,
-      sp.nombre AS SegundoPro,                        
-      c.Proexterno,                       
-      c.Descripcion,                       
-      c.IdTipoCurso,
-      tc.Especificacion AS IdTipoCurso,
-      e.nombre AS NombreProfesor,
-
-      -- Subconsulta JSON para traer los inscritos de cada curso
-      (
-        SELECT 
-          i.id,
-          i.idCur,
-          i.docInscr,
-          i.est,
-          i.fecreg,
-          (
-            SELECT 
-              n.Nota,
-              n.idRegistro,
-              n.FechaRegistro
-            FROM gescur.Notas n
-            WHERE n.IdCurso = i.idCur AND n.IdInscrito = i.id
-            FOR JSON PATH
-          ) AS Notas
-        FROM gescur.Inscripciones i
-        WHERE i.idCur = c.id AND i.est = 1
-        FOR JSON PATH
-      ) AS Inscritos
-
-    FROM gescur.cursos c
-    LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
-    LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
-    LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
-    LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
-    LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
-    LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
-    LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
-    LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
-  `);
-}
+[
+    {
+        "id": 450,
+        "NombreCurso": "Profesores Prub",
+        "Valor": null,
+        "Publico": "Profesores",
+        "Periodo": "2025-2",
+        "Inicio": "2025-04-30",
+        "Fin": "2025-05-10",
+        "InicioInscr": null,
+        "FinInscr": null,
+        "Horas": 5,
+        "CupoMax": 20,
+        "Lugar": "bloque",
+        "LunesIni": "10:51",
+        "LunesFin": "23:51",
+        "MartesIni": null,
+        "MartesFin": null,
+        "MiercolesIni": null,
+        "MiercolesFin": null,
+        "JuevesIni": null,
+        "JuevesFin": null,
+        "ViernesIni": null,
+        "ViernesFin": null,
+        "SabadoIni": null,
+        "SabadoFin": null,
+        "DomingoIni": null,
+        "DomingoFin": null,
+        "Linea": "Unidad de gestión externa",
+        "Estado": "Activo",
+        "Modalidad": "Presencial",
+        "Unidad": "Unidad de Proyectos                     ",
+        "Profesor": 1000034366,
+        "SegundoPro": "SUAREZ ESPINOSA JOHAN SMITH",
+        "Proexterno": null,
+        "Descripcion": null,
+        "IdTipoCurso": "Cursos BU y otros",
+        "NombreProfesor": "RODRIGUEZ REY WENDY SOFIA",
+        "Inscritos": "[{\"id\":1602,\"idCur\":450,\"docInscr\":1000034366,\"est\":true,\"fecreg\":\"2025-05-05\"}]"
+    },
+    {
