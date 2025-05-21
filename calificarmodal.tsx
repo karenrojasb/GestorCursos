5666b085fe23681d134f783943f3d9259a788c69
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 15:21:49 -0500

    Actualizar calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -3,58 +3,5 @@ interface Nota {
   idRegistro: number;
   FechaRegistro: string;
   NotaEspecificacion: string;
+  NombreRegistro: string; // nuevo campo
 }
-
-interface Inscrito {
-  FechaRegistro: any;
-  idRegistro: string;
-  id: number;
-  idCur: number;
-  docInscr: number;
-  est: boolean;
-  fecreg: string;
-  Notas?: Nota[];
-}
-
-interface Curso {
-  id: number;
-  NombreCurso: string;
-  Valor: number;
-  Publico: number;
-  Periodo: string;
-  Inicio: string;
-  Fin: string;
-  Horas: number;
-  CupoMax: number;
-  Lugar: string;
-  Linea: number;
-  Estado: number;
-  Modalidad: number;
-  Unidad: number;
-  Profesor: number;
-  SegundoPro: string;
-  Proexterno: string;
-  Descripcion: string;
-  IdTipoCurso: number;
-  NombreProfesor?: string;
-  LunesIni: string;
-  LunesFin: string;
-  MartesIni: string;
-  MartesFin: string;
-  MiercolesIni: string;
-  MiercolesFin: string;
-  JuevesIni: string;
-  JuevesFin: string;
-  ViernesIni: string;
-  ViernesFin: string;
-  SabadoIni: string;
-  SabadoFin: string;
-  DomingoIni: string;
-  DomingoFin: string;
-  InicioInscr: string;
-  FinInscr: string;
-
-  Inscritos ?: string;
-
-}
-
