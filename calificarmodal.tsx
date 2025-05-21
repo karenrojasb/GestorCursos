05fa05bad9cbadf312dd4ad2797cb0b0f1cd4c5e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 08:10:23 -0500

    Update calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -1,186 +1,57 @@
+ async getCourses() {
+    return this.prisma.$queryRaw`
+    SELECT 
+      c.id,
+      c.NombreCurso,
+      c.Valor,
+      lp.Especificacion AS Publico,
+      c.Periodo,
+      c.Inicio,
+      c.Fin,
+      c.InicioInscr,
+      c.FinInscr,
+      c.Horas,
+      c.CupoMax,
+      c.Lugar,                          
+      c.LunesIni,                          
+      c.LunesFin,                         
+      c.MartesIni,                       
+      c.MartesFin,                        
+      c.MiercolesIni,                      
+      c.MiercolesFin,                      
+      c.JuevesIni,                         
+      c.JuevesFin,                         
+      c.ViernesIni,                        
+      c.ViernesFin,                        
+      c.SabadoIni,                         
+      c.SabadoFin,                         
+      c.DomingoIni,                        
+      c.DomingoFin,                        
+      c.Linea,
+      l.Especificacion AS Linea,    
+      c.Estado,
+      est.Especificacion AS Estado, 
+      c.Modalidad,
+      m.Especificacion AS Modalidad,
+      u.nombre AS Unidad,                            
+      c.Profesor,                          
+      c.SegundoPro,
+      sp.nombre AS SegundoPro,                        
+      c.Proexterno,                       
+      c.Descripcion,                       
+      c.IdTipoCurso,
+      tc.Especificacion AS IdTipoCurso,
+      e.nombre AS NombreProfesor
+    FROM gescur.cursos c
+    LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
+    LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
+    LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
+    LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
+    LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
+    LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
+    LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
+    LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
 
-import { useState, useEffect } from "react";
-import { XMarkIcon } from "@heroicons/react/24/solid";
-
-interface CalificarModalProps {
-  nombre: string;
-  documento: number;
-  idCur: number;
-  idIns: number;
-  Especificacion: string;
-  onClose: () => void;
-  onGuardar: (nota: string) => void;
-}
-
-interface OpcionLista {
-  id: number;
-  Especificacion: string;
-}
-
-export default function CalificarModal({
-  nombre,
-  documento,
-  idCur,
-  idIns,
-  Especificacion,
-  onClose,
-  onGuardar,
-}: CalificarModalProps) {
-  const [opciones, setOpciones] = useState<OpcionLista[]>([]);
-  const [notaSeleccionada, setNotaSeleccionada] = useState<number | null>(null);
-  const [idEmp, setIdEmp] = useState<number | null>(null);
-  const [guardando, setGuardando] = useState(false);
-  const [notaExistenteId, setNotaExistenteId] = useState<number | null>(null);
-
-  useEffect(() => {
-    const storedId = localStorage.getItem("id_emp");
-    const parsedId = storedId ? Number(storedId) : null;
-    setIdEmp(parsedId);
-  }, []);
-
- 
-useEffect(() => {
-  const fetchDatos = async () => {
-    try {
-      const respOpciones = await fetch("http://localhost:8090/api/listas/Especificaciones");
-      if (!respOpciones.ok) throw new Error("Error al obtener lista de notas");
-      const dataOpciones = await respOpciones.json();
-      setOpciones(dataOpciones);
-
-      const respNota = await fetch(`http://localhost:8090/api/notas/curso-inscrito?idCurso=${idCur}&idInscrito=${documento}`);
-      if (respNota.ok) {
-        const dataNota = await respNota.json();
-        if (dataNota && dataNota.Nota !== undefined) {
-          setNotaSeleccionada(Number(dataNota.Nota));
-          setNotaExistenteId(dataNota.id);
-          console.log("Nota cargada:", dataNota.Nota);
-        }
-      }
-    } catch (error) {
-      console.error("Error cargando datos:", error);
-    }
-  };
-
-  if (idCur && documento) {
-    fetchDatos();
-  }
-}, [idCur, documento]);
-
-
-
-
-const handleGuardar = async () => {
-  if (notaSeleccionada === null || isNaN(notaSeleccionada)) {
-    alert("Por favor selecciona una nota válida");
-    return;
-  }
-
-  if (!idEmp) {
-    alert("Error: ID de empleado no encontrado.");
-    return;
-  }
-
-  setGuardando(true);
-
-  try {
-    const url = notaExistenteId
-      ? `http://localhost:8090/api/notas/${notaExistenteId}` // PUT
-      : "http://localhost:8090/api/notas"; // POST
-
-    const method = notaExistenteId ? "PUT" : "POST";
-
-    const response = await fetch(url, {
-      method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        idCurso: idCur,
-        idInscrito: documento,
-        idRegistro: idEmp,
-        Nota: notaSeleccionada,
-        FechaRegistro: new Date(),
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error al ${notaExistenteId ? "actualizar" : "crear"} la nota.`);
-    }
-
-    onGuardar(String(notaSeleccionada));
-    onClose();
-  } catch (error) {
-    console.error("Error al guardar nota:", error);
-    alert("Hubo un error al guardar la nota.");
-  } finally {
-    setGuardando(false);
-  }
-};
-
-
-  const especificacionNota = opciones.find(op => op.id === notaSeleccionada)?.Especificacion;
-
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-md w-[400px] relative">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-[#990000] transition-transform duration-300 transform hover:rotate-90 hover:scale-110"
-        >
-          <XMarkIcon className="h-6 w-6" />
-        </button>
-
-        <h2 className="text-2xl font-semibold text-[#990000] mb-4 text-center">Calificar</h2>
-
-        <p className="text-left mb-2"><strong>Nombre del Estudiante:</strong> {nombre}</p>
-        <p className="text-left mb-2"><strong>Nombre del Estudiante:</strong> {idIns}</p>
-        <p className="text-left mb-2"><strong>Documento:</strong> {documento}</p>
-      
-        
-
-        <div className="mb-4">
-          
-   
-<div className="mb-4">
-  <label className="block text-sm font-medium text-gray-700 mb-1">
-    Selecciona una calificación:
-  </label>
-
-  <select
-    className="w-full border rounded px-3 py-2"
-    value={notaSeleccionada ?? ""}
-    onChange={(e) => setNotaSeleccionada(Number(e.target.value))}
-  >
-    <option value="">-- Selecciona --</option>
-    {opciones.map((op) => (
-      <option key={op.id} value={op.id}>
-        {op.Especificacion}
-      </option>
-    ))}
-  </select>
-
- 
-</div>
-
-
-        </div>
-
-        <div className="flex justify-center gap-4">
-          <button
-            onClick={handleGuardar}
-            disabled={guardando}
-            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition hover:scale-110 active:scale-95"
-          >
-            {guardando ? "Guardando..." : "Guardar"}
-          </button>
-          <button
-            onClick={onClose}
-            className="bg-[#990000] text-white px-4 py-2 rounded hover:bg-red-700 transition hover:scale-110 active:scale-95"
-          >
-            Cancelar
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    ;
+    `
 }
