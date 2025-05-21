545852039b0074ecdaee09cc6222365fbe5908f9
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 15:26:20 -0500

    Actualizar calificarmodal.tsx

--- calificarmodal.tsx
+++ calificarmodal.tsx
@@ -3,7 +3,7 @@
     <table className="min-w-full text-[0.8rem] table-fixed border border-gray-300 bg-white shadow-md rounded-lg">
       <thead>
         <tr className="bg-[#990000] text-white">
-          <th colSpan={8} className="text-center py-2 text-base font-semibold border-b">
+          <th colSpan={9} className="text-center py-2 text-base font-semibold border-b">
             Inscritos en este curso
           </th>
         </tr>
@@ -11,9 +11,9 @@
           <th className="px-3 py-1 border">ID</th>
           <th className="px-3 py-1 border">ID Curso</th>
           <th className="px-3 py-1 border">Documento</th>
+          <th className="px-3 py-1 border">Nombre del Inscrito</th>
           <th className="px-3 py-1 border">Estado</th>
           <th className="px-3 py-1 border">Fecha de Inscripción</th>
-          
           <th className="px-3 py-1 border">Calificador</th>
           <th className="px-3 py-1 border">Fecha de Calificación</th>
           <th className="px-3 py-1 border">Nota</th>
@@ -27,10 +27,12 @@
             <tr key={inscrito.id} className="text-gray-700 text-center">
               <td className="px-3 py-1 border">{inscrito.id}</td>
               <td className="px-3 py-1 border">{inscrito.idCur}</td>
+              <td className="px-3 py-1 border">{inscrito.docInscr}</td>
               <td className="px-3 py-1 border">{inscrito.NombreInscrito}</td>
               <td className="px-3 py-1 border">{inscrito.est ? "Activo" : "Inactivo"}</td>
-              <td className="px-3 py-1 border">{new Date(inscrito.fecreg).toLocaleDateString()}</td>
-              
+              <td className="px-3 py-1 border">
+                {new Date(inscrito.fecreg).toLocaleDateString()}
+              </td>
               <td className="px-3 py-1 border">{nota.NombreRegistro ?? "—"}</td>
               <td className="px-3 py-1 border">
                 {nota.FechaRegistro
