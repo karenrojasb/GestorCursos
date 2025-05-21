4e8d25ed17d94c4a4470a5706eade5c059a2594c
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 11:23:16 -0500

    Update Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,14 +1,33 @@
-interface Nota {
-  Nota: number;
-  idRegistro: number;
-  FechaRegistro: string;
-}
-
-interface Inscrito {
-  id: number;
-  idCur: number;
-  docInscr: number;
-  est: boolean;
-  fecreg: string;
-  Notas?: Nota[];
-}
+{curso.Inscritos && curso.Inscritos !== "[]" && (
+      <div className="mt-2 w-full">
+        <table className="min-w-full text-[0.8rem] table-fixed border border-gray-300 bg-white shadow-md rounded-lg">
+          <thead>
+            <tr className="bg-[#990000] text-white">
+              <th colSpan={5} className="text-center py-2 text-base font-semibold border-b">
+                Inscritos en este curso
+              </th>
+            </tr>
+            <tr className="bg-gray-100 text-[#990000] font-medium">
+              <th className="px-3 py-1 border">ID</th>
+              <th className="px-3 py-1 border">ID Curso</th>
+              <th className="px-3 py-1 border">Documento</th>
+              <th className="px-3 py-1 border">Estado</th>
+              <th className="px-3 py-1 border">Fecha Registro</th>
+            </tr>
+          </thead>
+          <tbody>
+            {JSON.parse(curso.Inscritos).map((inscrito: any) => (
+              <tr key={inscrito.id} className="text-gray-700 text-center">
+                <td className="px-3 py-1 border">{inscrito.id}</td>
+                <td className="px-3 py-1 border">{inscrito.idCur}</td>
+                <td className="px-3 py-1 border">{inscrito.docInscr}</td>
+                <td className="px-3 py-1 border">{inscrito.est}</td>
+                <td className="px-3 py-1 border">{new Date(inscrito.fecreg).toLocaleDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        
+       
+      </div>
+    )}
