ded438ae6023d09e6f54d191beb7f548f9c0d9e6
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 10:35:59 -0500

    Update Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,79 +1,42 @@
-async getCourses() {
-  return this.prisma.$queryRawUnsafe(`
-    SELECT 
-      c.id,
-      c.NombreCurso,
-      c.Valor,
-      lp.Especificacion AS Publico,
-      c.Periodo,
-      c.Inicio,
-      c.Fin,
-      c.InicioInscr,
-      c.FinInscr,
-      c.Horas,
-      c.CupoMax,
-      c.Lugar,                          
-      c.LunesIni,                          
-      c.LunesFin,                         
-      c.MartesIni,                       
-      c.MartesFin,                        
-      c.MiercolesIni,                      
-      c.MiercolesFin,                      
-      c.JuevesIni,                         
-      c.JuevesFin,                         
-      c.ViernesIni,                        
-      c.ViernesFin,                        
-      c.SabadoIni,                         
-      c.SabadoFin,                         
-      c.DomingoIni,                        
-      c.DomingoFin,                        
-      c.Linea,
-      l.Especificacion AS Linea,    
-      c.Estado,
-      est.Especificacion AS Estado, 
-      c.Modalidad,
-      m.Especificacion AS Modalidad,
-      u.nombre AS Unidad,                            
-      c.Profesor,                          
-      c.SegundoPro,
-      sp.nombre AS SegundoPro,                        
-      c.Proexterno,                       
-      c.Descripcion,                       
-      c.IdTipoCurso,
-      tc.Especificacion AS IdTipoCurso,
-      e.nombre AS NombreProfesor,
 
-      (
-        SELECT 
-          i.id,
-          i.idCur,
-          i.docInscr,
-          i.est,
-          i.fecreg,
-          JSON_QUERY(
-            (
-              SELECT 
-                n.Nota,
-                n.idRegistro,
-                n.FechaRegistro
-              FROM gescur.Notas n
-              WHERE n.IdCurso = i.idCur AND n.IdInscrito = i.docInscr
-              FOR JSON PATH
-            )
-          ) AS Notas
-        FROM gescur.Inscripciones i
-        WHERE i.idCur = c.id AND i.est = 1
-        FOR JSON PATH
-      ) AS Inscritos
+interface Curso {
+  id: number;
+  NombreCurso: string;
+  Valor: number;
+  Publico: number;
+  Periodo: string;
+  Inicio: string;
+  Fin: string;
+  Horas: number;
+  CupoMax: number;
+  Lugar: string;
+  Linea: number;
+  Estado: number;
+  Modalidad: number;
+  Unidad: number;
+  Profesor: number;
+  SegundoPro: string;
+  Proexterno: string;
+  Descripcion: string;
+  IdTipoCurso: number;
+  NombreProfesor?: string;
+  LunesIni: string;
+  LunesFin: string;
+  MartesIni: string;
+  MartesFin: string;
+  MiercolesIni: string;
+  MiercolesFin: string;
+  JuevesIni: string;
+  JuevesFin: string;
+  ViernesIni: string;
+  ViernesFin: string;
+  SabadoIni: string;
+  SabadoFin: string;
+  DomingoIni: string;
+  DomingoFin: string;
+  InicioInscr: string;
+  FinInscr: string;
 
-    FROM gescur.cursos c
-    LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
-    LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
-    LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
-    LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
-    LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
-    LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
-    LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
-    LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
-  `);
+  Inscritos ?: string;
+
 }
