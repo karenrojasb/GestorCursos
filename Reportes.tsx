17025b64d596f52ee37fcca48fa16102b293703e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 10:32:58 -0500

    Update Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,79 +1,39 @@
-async getCourses() {
-  return this.prisma.$queryRawUnsafe(`
-    SELECT 
-      c.id,
-      c.NombreCurso,
-      c.Valor,
-      lp.Especificacion AS Publico,
-      c.Periodo,
-      c.Inicio,
-      c.Fin,
-      c.InicioInscr,
-      c.FinInscr,
-      c.Horas,
-      c.CupoMax,
-      c.Lugar,                          
-      c.LunesIni,                          
-      c.LunesFin,                         
-      c.MartesIni,                       
-      c.MartesFin,                        
-      c.MiercolesIni,                      
-      c.MiercolesFin,                      
-      c.JuevesIni,                         
-      c.JuevesFin,                         
-      c.ViernesIni,                        
-      c.ViernesFin,                        
-      c.SabadoIni,                         
-      c.SabadoFin,                         
-      c.DomingoIni,                        
-      c.DomingoFin,                        
-      c.Linea,
-      l.Especificacion AS Linea,    
-      c.Estado,
-      est.Especificacion AS Estado, 
-      c.Modalidad,
-      m.Especificacion AS Modalidad,
-      u.nombre AS Unidad,                            
-      c.Profesor,                          
-      c.SegundoPro,
-      sp.nombre AS SegundoPro,                        
-      c.Proexterno,                       
-      c.Descripcion,                       
-      c.IdTipoCurso,
-      tc.Especificacion AS IdTipoCurso,
-      e.nombre AS NombreProfesor,
-
-      (
-        SELECT 
-          i.id,
-          i.idCur,
-          i.docInscr,
-          i.est,
-          i.fecreg,
-          JSON_QUERY(
-            (
-              SELECT 
-                n.Nota,
-                n.idRegistro,
-                n.FechaRegistro
-              FROM gescur.Notas n
-              WHERE n.IdCurso = i.idCur AND n.IdInscrito = i.id
-              FOR JSON PATH
-            )
-          ) AS Notas
-        FROM gescur.Inscripciones i
-        WHERE i.idCur = c.id AND i.est = 1
-        FOR JSON PATH
-      ) AS Inscritos
-
-    FROM gescur.cursos c
-    LEFT JOIN gescur.listas lp ON lp.id = c.Publico AND lp.Tipo = 1
-    LEFT JOIN gescur.listas l ON l.id = c.Linea AND l.Tipo = 2
-    LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
-    LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
-    LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
-    LEFT JOIN gescur.emp_nomina e ON c.Profesor = e.id_emp    
-    LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
-    LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
-  `);
-}
+    {
+        "id": 306,
+        "NombreCurso": "Matemática financiera básica",
+        "Valor": 0,
+        "Publico": "Todos",
+        "Periodo": "2024-1",
+        "Inicio": "2024-06-25",
+        "Fin": "2024-07-18",
+        "InicioInscr": null,
+        "FinInscr": null,
+        "Horas": 16,
+        "CupoMax": 20,
+        "Lugar": "G-106",
+        "LunesIni": "",
+        "LunesFin": "",
+        "MartesIni": "14:00",
+        "MartesFin": "16:00",
+        "MiercolesIni": "",
+        "MiercolesFin": "",
+        "JuevesIni": "14:00",
+        "JuevesFin": "16:00",
+        "ViernesIni": "",
+        "ViernesFin": "",
+        "SabadoIni": "",
+        "SabadoFin": "",
+        "DomingoIni": "",
+        "DomingoFin": "",
+        "Linea": "Cursos generales",
+        "Estado": "Activo",
+        "Modalidad": "Presencial",
+        "Unidad": "Vicerrectoría Académica                 ",
+        "Profesor": 1032402874,
+        "SegundoPro": null,
+        "Proexterno": "",
+        "Descripcion": "Matemática financiera básica",
+        "IdTipoCurso": "Desarrollo profesoral",
+        "NombreProfesor": "ROJAS BELTRAN KAREN DAYANA",
+        "Inscritos": "[{\"id\":1400,\"idCur\":306,\"docInscr\":1019015486,\"est\":true,\"fecreg\":\"2024-06-05\"},{\"id\":1401,\"idCur\":306,\"docInscr\":51970259,\"est\":true,\"fecreg\":\"2024-06-05\"},{\"id\":1404,\"idCur\":306,\"docInscr\":1070307337,\"est\":true,\"fecreg\":\"2024-06-05\"},{\"id\":1405,\"idCur\":306,\"docInscr\":1018499612,\"est\":true,\"fecreg\":\"2024-06-05\"},{\"id\":1406,\"idCur\":306,\"docInscr\":35415516,\"est\":true,\"fecreg\":\"2024-06-05\"},{\"id\":1409,\"idCur\":306,\"docInscr\":53062434,\"est\":true,\"fecreg\":\"2024-06-06\"},{\"id\":1417,\"idCur\":306,\"docInscr\":79788651,\"est\":true,\"fecreg\":\"2024-06-07\"},{\"id\":1418,\"idCur\":306,\"docInscr\":1032448343,\"est\":true,\"fecreg\":\"2024-06-07\"},{\"id\":1435,\"idCur\":306,\"docInscr\":79718651,\"est\":true,\"fecreg\":\"2024-06-14\"},{\"id\":1442,\"idCur\":306,\"docInscr\":18125616,\"est\":true,\"fecreg\":\"2024-06-19\"}]"
+    },
