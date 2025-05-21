e4cc4d6a668ea11b176bca2311d4a0068b4c06cf
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 16:35:12 -0500

    Actualizar Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,11 +1,10 @@
 <select
   className="border rounded px-2 py-1 text-sm bg-white"
   value={
-    opciones.find((op) => op.Especificacion === insc.Especificacion)?.id || ""
+    // Buscar el id que corresponde al texto que tiene la inscripción en 'Especificacion'
+    opciones.find(op => op.Especificacion === insc.Especificacion)?.id || ""
   }
-  onChange={(e) =>
-    handleChangeEspecificacion(insc.id, Number(e.target.value))
-  }
+  onChange={(e) => handleChangeEspecificacion(insc.id, Number(e.target.value))}
 >
   <option value="">-- Selecciona --</option>
   {opciones.map((opcion) => (
