6e8f1b831935d3fc44df9bc491ed62da156056e4
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 11:22:38 -0500

    Actualizar Reportes.tsx

--- Reportes.tsx
+++ Reportes.tsx
@@ -1,42 +1,14 @@
+interface Nota {
+  Nota: number;
+  idRegistro: number;
+  FechaRegistro: string;
+}
 
-interface Curso {
+interface Inscrito {
   id: number;
-  NombreCurso: string;
-  Valor: number;
-  Publico: number;
-  Periodo: string;
-  Inicio: string;
-  Fin: string;
-  Horas: number;
-  CupoMax: number;
-  Lugar: string;
-  Linea: number;
-  Estado: number;
-  Modalidad: number;
-  Unidad: number;
-  Profesor: number;
-  SegundoPro: string;
-  Proexterno: string;
-  Descripcion: string;
-  IdTipoCurso: number;
-  NombreProfesor?: string;
-  LunesIni: string;
-  LunesFin: string;
-  MartesIni: string;
-  MartesFin: string;
-  MiercolesIni: string;
-  MiercolesFin: string;
-  JuevesIni: string;
-  JuevesFin: string;
-  ViernesIni: string;
-  ViernesFin: string;
-  SabadoIni: string;
-  SabadoFin: string;
-  DomingoIni: string;
-  DomingoFin: string;
-  InicioInscr: string;
-  FinInscr: string;
-
-  Inscritos ?: string;
-
+  idCur: number;
+  docInscr: number;
+  est: boolean;
+  fecreg: string;
+  Notas?: Nota[];
 }
