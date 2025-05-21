23fecbc8d261d260a0b8c65f0231cd983e9ee77a
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-21 15:46:04 -0500

    Actualizar Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,21 +1,38 @@
- const fetchCursos = async () => {
-    setIsLoading(true);
-    try {
-      const response = await fetch("http://localhost:8090/api/cursos");
-      if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
-      const data = await response.json();
-      setCursos(data);
-      setCursosFiltrados(data);
-    } catch (error) {
-      console.error("Error al obtener los cursos:", error);
-    }
-    setIsLoading(false);
-  };
+const [cursos, setCursos] = useState([]);
+const [cursosFiltrados, setCursosFiltrados] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+const [especificaciones, setEspecificaciones] = useState([]);
 
-  useEffect(() => {
-    fetchCursos();
-  }, []);
+const fetchCursos = async () => {
+  setIsLoading(true);
+  try {
+    const response = await fetch("http://localhost:8090/api/cursos");
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+    const data = await response.json();
+    setCursos(data);
+    setCursosFiltrados(data);
+  } catch (error) {
+    console.error("Error al obtener los cursos:", error);
+  }
+  setIsLoading(false);
+};
 
-  const handleUpdate = () => {
-    fetchCursos();
-  };
+const fetchEspecificaciones = async () => {
+  try {
+    const response = await fetch("http://localhost:8090/api/listas/especificacion");
+    if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
+    const data = await response.json();
+    setEspecificaciones(data);
+  } catch (error) {
+    console.error("Error al obtener las especificaciones:", error);
+  }
+};
+
+useEffect(() => {
+  fetchCursos();
+  fetchEspecificaciones();
+}, []);
+
+const handleUpdate = () => {
+  fetchCursos();
+};
