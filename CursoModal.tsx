dd3b971a036a128cd628d049ddb2ae4267c74e00
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-23 11:47:17 -0500

    Actualizar CursoModal.tsx

--- CursoModal.tsx
+++ CursoModal.tsx
@@ -12,33 +12,33 @@ async getCourses() {
       c.FinInscr,
       c.Horas,
       c.CupoMax,
-      c.Lugar,                          
-      c.LunesIni,                          
-      c.LunesFin,                         
-      c.MartesIni,                       
-      c.MartesFin,                        
-      c.MiercolesIni,                      
-      c.MiercolesFin,                      
-      c.JuevesIni,                         
-      c.JuevesFin,                         
-      c.ViernesIni,                        
-      c.ViernesFin,                        
-      c.SabadoIni,                         
-      c.SabadoFin,                         
-      c.DomingoIni,                        
-      c.DomingoFin,                        
+      c.Lugar,
+      c.LunesIni,
+      c.LunesFin,
+      c.MartesIni,
+      c.MartesFin,
+      c.MiercolesIni,
+      c.MiercolesFin,
+      c.JuevesIni,
+      c.JuevesFin,
+      c.ViernesIni,
+      c.ViernesFin,
+      c.SabadoIni,
+      c.SabadoFin,
+      c.DomingoIni,
+      c.DomingoFin,
       c.Linea AS LineaId,
-      l.Especificacion AS LineaNombre,    
+      l.Especificacion AS LineaNombre,
       c.Estado AS EstadoId,
-      est.Especificacion AS EstadoNombre, 
+      est.Especificacion AS EstadoNombre,
       c.Modalidad AS ModalidadId,
       m.Especificacion AS ModalidadNombre,
-      u.nombre AS Unidad,                            
-      c.Profesor,                          
+      u.nombre AS Unidad,
+      c.Profesor,
       c.SegundoPro,
-      sp.nombre AS SegundoProNombre,                        
-      c.Proexterno,                       
-      c.Descripcion,                       
+      sp.nombre AS SegundoProNombre,
+      c.Proexterno,
+      c.Descripcion,
       c.IdTipoCurso,
       tc.Especificacion AS TipoCursoNombre,
       e.nombre AS NombreProfesor,
@@ -61,13 +61,13 @@ async getCourses() {
                 emp.nombre AS NombreRegistro
               FROM gescur.Notas n
               LEFT JOIN gescur.Listas li ON li.id = n.Nota AND li.Tipo = 9
-              LEFT JOIN gescur.emp_nomina emp ON CAST(emp.id_emp AS VARCHAR) = LTRIM(RTRIM(n.idRegistro))
+              LEFT JOIN gescur.emp_nomina emp ON LTRIM(RTRIM(CAST(emp.id_emp AS VARCHAR))) = LTRIM(RTRIM(n.idRegistro))
               WHERE n.IdCurso = i.idCur AND n.IdInscrito = i.docInscr
               FOR JSON PATH
             )
           ) AS Notas
         FROM gescur.Inscripciones i
-        LEFT JOIN gescur.emp_nomina en ON CAST(en.id_emp AS VARCHAR) = LTRIM(RTRIM(i.docInscr))
+        LEFT JOIN gescur.emp_nomina en ON LTRIM(RTRIM(CAST(en.id_emp AS VARCHAR))) = LTRIM(RTRIM(i.docInscr))
         WHERE i.idCur = c.id AND i.est = 1
         FOR JSON PATH
       ) AS Inscritos
@@ -78,8 +78,8 @@ async getCourses() {
     LEFT JOIN gescur.listas m ON m.id = c.Modalidad AND m.Tipo = 3
     LEFT JOIN gescur.listas est ON est.id = c.Estado AND est.Tipo = 4
     LEFT JOIN gescur.listas tc ON tc.id = c.IdTipoCurso AND tc.Tipo = 8
-     LEFT JOIN gescur.emp_nomina e ON CAST(c.Profesor AS VARCHAR) = e.id_emp
-     LEFT JOIN gescur.emp_nomina sp ON CAST(c.SegundoPro AS VARCHAR) = sp.id_emp
+    LEFT JOIN gescur.emp_nomina e ON LTRIM(RTRIM(CAST(e.id_emp AS VARCHAR))) = LTRIM(RTRIM(CAST(c.Profesor AS VARCHAR)))
+    LEFT JOIN gescur.emp_nomina sp ON LTRIM(RTRIM(CAST(sp.id_emp AS VARCHAR))) = LTRIM(RTRIM(CAST(c.SegundoPro AS VARCHAR)))
     LEFT JOIN gescur.unidad u ON c.Unidad = u.codigo
   `);
 }
