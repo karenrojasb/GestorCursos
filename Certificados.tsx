1714e37281a778358ae259a3902a1d3cc6241e9e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-27 14:45:17 -0500

    Update Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,4 +1,65 @@
-async createRegistration(data: CreateInscripcionDto) {
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service'; // o el nombre que tengas
+import { CreateAuditoriaNotaDto } from './dto/create-auditorianotas.dto';
+
+@Injectable()
+export class AuditoriaNotasService {
+  constructor(private prisma: PrismaService) {}
+
+async createAuditoria(CreateAuditoriaNotaDto: CreateAuditoriaNotaDto) {
+  console.log('DTO recibido:', CreateAuditoriaNotaDto);
+
+  try {
+    
+    const notaExistente = await this.prisma.auditoriasNotas.findFirst({
+      where: {
+        idCurso: CreateAuditoriaNotaDto.idCurso,
+        idInscrito: CreateAuditoriaNotaDto.idInscrito,
+      },
+    });
+
+    if (notaExistente) {
+   
+
+      const nuevaNotaMasCompleta =
+        CreateAuditoriaNotaDto.Nota !== null &&
+        CreateAuditoriaNotaDto.Nota !== undefined &&
+       CreateAuditoriaNotaDto.Nota !== notaExistente.Nota;
+
+      if (nuevaNotaMasCompleta) {
+        const notaActualizada = await this.prisma.auditoriasNotas.update({
+          where: { id: notaExistente.id },
+          data: {
+            Nota: CreateAuditoriaNotaDto
+            .Nota,
+            idRegistro: CreateAuditoriaNotaDto.idRegistro,
+            
+          },
+        });
+        return notaActualizada;
+      } else {
+      
+        return notaExistente;
+      }
+    } else {
+   
+      const newNote = await this.prisma.notas.create({
+        data: {
+          idCurso: CreateAuditoriaNotaDto.idCurso,
+          idInscrito: CreateAuditoriaNotaDto.idInscrito,
+          Nota: CreateAuditoriaNotaDto.Nota,
+          idRegistro: CreateAuditoriaNotaDto.idRegistro,
+          FechaRegistro: new Date(),
+        },
+      });
+      return newNote;
+    }
+  } catch (error) {
+    console.error('Error al crear la nota:', error);
+    throw new Error(`No se pudo crear la nota: ${error.message}`);
+  }
+}
+}async createRegistration(data: CreateInscripcionDto) {
     const inscripcionExistente = await this.prisma.inscripciones.findFirst({
       where: {
         idCur: data.idCur,
