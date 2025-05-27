0349b1664580263d3f72c72fcd95411ee73b4381
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-27 14:50:10 -0500

    Update Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,6 +1,2 @@
-export class CreateAuditoriaNotaDto {
-  idCurso: number;
-  idInscrito: number;
-  Nota: number;
-  idRegistro: number;
-}
+exType 'Date' is not assignable to type 'string | StringFieldUpdateOperationsInput | undefined'.ts(2322)
+index.d.ts(10171, 5): The expected type comes from property 'FechaRegistro' which is declared here on type '(Without<AuditoriasNotasUpdateInput, AuditoriasNotasUncheckedUpdateInput> & AuditoriasNotasUncheckedUpdateInput) | (Without<...> & AuditoriasNotasUpdateInput)'
