8e2c668ea76e5ad1f8d35c343c5dd63226c7876b
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-28 14:45:18 -0500

    Actualizar Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,1 +1,23 @@
- <span className=" text-center text-gray-700">{curso.EstadoNombre}</span>
+<span className="flex items-center justify-center gap-1 text-gray-700">
+  {curso.EstadoNombre === 'Activo' && (
+    <>
+      <span className="w-2.5 h-2.5 bg-green-500 rounded-full" title="Activo"></span>
+      <span>{curso.EstadoNombre}</span>
+    </>
+  )}
+  {curso.EstadoNombre === 'Cancelado' && (
+    <>
+      <span className="w-2.5 h-2.5 bg-red-500 rounded-full" title="Cancelado"></span>
+      <span>{curso.EstadoNombre}</span>
+    </>
+  )}
+  {curso.EstadoNombre === 'Terminado' && (
+    <>
+      <span className="w-2.5 h-2.5 bg-gray-500 rounded-full" title="Terminado"></span>
+      <span>{curso.EstadoNombre}</span>
+    </>
+  )}
+</span>
+
+
+
