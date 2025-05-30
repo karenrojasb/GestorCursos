be24f0dcf27450dc1122b02c09bd50afa8886eae
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-05-30 08:23:01 -0500

    Actualizar Certificados.tsx

--- Certificados.tsx
+++ Certificados.tsx
@@ -1,25 +1,25 @@
 <body className="relative flex flex-col min-h-screen bg-gray-100 overflow-hidden">
 
-  {/* Fondo decorativo gris (z-index -1) */}
+  {/* Fondo decorativo gris (z-index -2) */}
   <div
-    className="absolute bottom-0 right-0 w-[700px] h-[500px] z-[-2]"
+    className="absolute bottom-0 right-0 w-[900px] h-[650px] z-[-2]"
     style={{
       backgroundColor: '#f3f4f6',
-      clipPath: "path('M700,0 Q400,600 0,500 L0,0 Z')",
+      clipPath: "path('M900,0 Q500,800 0,650 L0,0 Z')",
     }}
   ></div>
 
-  {/* Imagen de fondo (también con z-[-1]) */}
+  {/* Imagen de fondo (z-index -3) */}
   <div
-    className="absolute bottom-0 right-0 w-[700px] h-[500px] bg-no-repeat bg-cover z-[-3]"
+    className="absolute bottom-0 right-0 w-[900px] h-[650px] bg-no-repeat bg-cover z-[-3]"
     style={{
       backgroundImage: "url('/img/_DSC0764-3.jpg')",
-      clipPath: "path('M0,0 L700,0 L700,500 L0,500 Z M700,0 Q400,600 0,500 L0,0 Z')",
+      clipPath: "path('M0,0 L900,0 L900,650 L0,650 Z M900,0 Q500,800 0,650 L0,0 Z')",
       clipRule: "evenodd",
     }}
   ></div>
 
-  {/* Contenido de la app (z-index superior) */}
+  {/* Contenido principal */}
   <header className="z-10 w-full flex items-center justify-between px-6 py-2 bg-white shadow-md">
     ...
   </header>
